4505668bac1e9c124de6ae297532864242180023
littleze <1585711131@qq.com>
2021-05-22 20:55:45 +0800

    初始化

---
+++ leetcode/editor/cn/[剑指 Offer 05]替换空格.java
@@ -0,0 +1,34 @@
+package editor.cn;//请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+//
+// 
+//
+// 示例 1： 
+//
+// 输入：s = "We are happy."
+//输出："We%20are%20happy." 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= s 的长度 <= 10000 
+// 👍 120 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String replaceSpace(String s) {
+        // return s.replace(" ", "%20");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (ch != ' ') {
+                sb.append(ch);
+                continue;
+            }
+            sb.append("%20");
+        }
+        return new String(sb);
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
