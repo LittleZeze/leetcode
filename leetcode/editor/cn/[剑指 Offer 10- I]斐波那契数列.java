4ad8391d683a0d684e0c6b02e4411481d203ffa9
zhaogengze <1585711131@qq.com>
2021-06-01 10:46:24 +0800

    add

--- leetcode/editor/cn/[剑指 Offer 10- I]斐波那契数列.java
+++ leetcode/editor/cn/[剑指 Offer 10- I]斐波那契数列.java
@@ -1,4 +1,4 @@
-//写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下： 
+//写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
 //
 // 
 //F(0) = 0,   F(1) = 1
@@ -37,8 +37,38 @@
 
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
+
+
     public int fib(int n) {
 
+        if (n < 2) {
+            return n;
+        }
+
+        long[] dp = new long[101];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+            dp[i] %= (Math.pow(10, 9) + 7);
+        }
+
+        return (int)dp[n];
+
+        /*// 不能用递归，会超时
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+
+        int result = fib(n - 1) + fib(n - 2);
+        if (result == 1000000008) {
+            return 1;
+        } else {
+            return result;
+        }*/
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
